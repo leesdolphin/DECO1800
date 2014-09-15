f97985b4d3e0630e43853832daacb421c77244c8
Lee Symes <leesdolphin@gmail.com>
2014-09-15 15:12:35 +1000

    no message

---
+++ include/head.php
@@ -0,0 +1,6 @@
+<meta charset="UTF-8">
+<link rel="stylesheet" href="css/bootstrap.min.css" />
+<link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+<link rel="stylesheet" href="css/jquery-ui.min.css" />
+<script src="js/jquery-2.1.1.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
