35c6f04acbdae595f2e24dea78c656de13d6c082
LukeAbel <luke.abel@uqconnect.edu.au>
2014-09-24 12:25:45 +1000

    Auto complete Database Query Testing

--- autocomplete.php
+++ autocomplete.php
@@ -10,12 +10,21 @@ if (mysqli_connect_errno()) {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
 
-$result = mysqli_query($con,'SELECT TownName FROM TOWNPOP WHERE TownName="'. $_GET[0].'%"');
+/*$result = mysqli_query($con,'SELECT TownName FROM TOWNPOP WHERE TownName="'. $_GET[0].'%"');
 
 while($row = mysqli_fetch_array($result)) {
     echo 'I have no idea what I am doing!!!!!';
     // I have no idea what I am doing!!!!!
 }
+*/
 
+$result = mysqli_query($con,"SELECT TownName FROM TOWNPOP");
+			echo '<div id="res">';
+			while($row = mysqli_fetch_array($result)) {
+				echo '<div class="town">';
+				echo $row['TownName'];
+				echo "</div>";
+			}
+			echo "</div>";
 mysqli_close($con);
 ?>
