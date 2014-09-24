70ebace92815b302fa8461aa6529c04be1f4e7c4
Lee Symes <leesdolphin@gmail.com>
2014-09-24 16:39:14 +1000

    Cleaned up autocomplete.php; Just going to test it now.

--- autocomplete.php
+++ autocomplete.php
@@ -4,27 +4,52 @@ $dbhost = "localhost";
 $dbuser = "site";
 $dbpass = "pass";
 $dbname = "TOWN";
-	
+
 $con = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 if (mysqli_connect_errno()) {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit;
 }
 
-/*$result = mysqli_query($con,'SELECT TownName FROM TOWNPOP WHERE TownName="'. $_GET[0].'%"');
+/* 
+$result = mysqli_query($con,'SELECT TownName FROM TOWNPOP WHERE TownName="'. $_GET[0].'%"');
 
-while($row = mysqli_fetch_array($result)) {
-    echo 'I have no idea what I am doing!!!!!';
-    // I have no idea what I am doing!!!!!
+  while($row = mysqli_fetch_array($result)) {
+  echo 'I have no idea what I am doing!!!!!';
+  // I have no idea what I am doing!!!!!
+  }
+ */
+/**
+ * This prepares a statement.
+ * Then binds the values.
+ * Executes it
+ * Then binds the `TownName` value(from the database) for each row to `$townName`
+ */
+$stmt = $con->prepare("SELECT TownName FROM TOWNPOP WHERE TownName LIKE ? Order By TownName asc");
+$stmt->bind_param("s", $_GET["term"]);
+$stmt->execute();
+$stmt->bind_result($townName);
+
+echo '[';
+if($stmt->fetch()) {
+    /**
+     * There are results; the first one is now in `$townName
+     */
+    echo json_encode($townName);
+    while ($stmt->fetch()) {
+        /**
+         * Now do the rest of them
+         */
+        echo ", " . json_encode($townName);
+    }
 }
-*/
+echo "]";
 
-$result = mysqli_query($con,"SELECT TownName FROM TOWNPOP");
-			echo '<div id="res">';
-			while($row = mysqli_fetch_array($result)) {
-				echo '<div class="town">';
-				echo $row['TownName'];
-				echo "</div>";
-			}
-			echo "</div>";
-mysqli_close($con);
+/**
+ * Now clean up our mess and close the connection.
+ */
+$stmt->free_result();
+$stmt->close();
+$con->close();
+
 ?>
